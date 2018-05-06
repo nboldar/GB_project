7df0468e08e57a125ecd936251b20c749b98e3e6
nboldar <nboldar1@gmail.com>
2018-05-06 12:24:49 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var less = require('gulp-less'); // компиляция less файлов
+var browserSync = require('browser-sync'); // создание и обновление локального сервера
+var autoprefixer = require('autoprefixer');// расставляет префиксы в css файлах
+var gulpAutoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var csso = require('gulp-csso');
+var csscss = require('gulp-csscss')
+var cleanCss=require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var htmlmin = require('gulp-htmlmin');
+var htmlhint = require('gulp-htmlhint');
+var imagemin = require('gulp-imagemin');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+// var uglifyjs = require('gulp-uglifyjs');
+var uncss = require('gulp-uncss');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+var shorthand= require('gulp-shorthand');
+var cssnano = require('cssnano'); // минификатор сss в рамках postcss
+var mQueries=require('gulp-group-css-media-queries');
+var reload = browserSync.reload();
+var paths = {
+    html: ['index.html'],
+    css: ['index.css'],
+    script: ['my-script.js']
+};
+
+gulp.task('imagemin', function () {
+    return gulp.src('app/img/**/*')
+        .pipe(imagemin({progressive: true}))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html', function () {
+    return gulp.src('app/*.html')
+        .pipe(htmlhint())
+        .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('csso', function () {
+    return gulp.src(['app/css/checkout1.css','app/css/index.css','app/css/product.css','app/css/shoping_cart.css','app/css/single_page.css'])
+        //
+        .pipe(uncss({
+            html: ['app/*.html']
+        }))
+        .pipe(gulpAutoprefixer({browsers: ['cover 99.5%']}))
+        .pipe(mQueries())
+        .pipe(csso())
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('autoprefixer', function () {
+    return gulp.src('app/css/*.css')
+        .pipe(gulpAutoprefixer({browsers: ['cover 99.5%']}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('less', function () {
+    return gulp.src('app/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('app/css'));
+});
+
+
+
+gulp.task('exitServe', function () {
+    browserSync.exit();
+});
+
+
